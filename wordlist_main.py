8371f5e294dff44c2b136a45eb227bbec2085b98
CorbetQ <qcorbet@gmail.com>
2024-05-16 15:28:30 +0200

    Add files

--- wordlist_main.py
+++ wordlist_main.py
@@ -1,4 +1,7 @@
 from os import system
+import wordlist_processor
+
+output = input("Add your output folder : ")
 
 def wordlist():
     print(" _____________________________ ")
@@ -16,10 +19,13 @@ def wordlist():
     department   = input("Your department (nb)    : ")
     country      = input("Your country            : ")
     partner_name = input("Your partner's name     : ")
+    partner_sname= input("Your partner's s. name  : ")
+    partner_tname= input("Your partner's t. name  : ")
     partner_fname= input("Your partner's f. name  : ")
-    partner_sname= input("Your partner's surname  : ")
+    partner_srnm = input("Your partner's surname  : ")
     partner_birth= input("Your partner's birthday : ")
     kid_name     = input("Your kid's name         : ")
+    kid_surname  = input("Your kid's surname      : ")
     kid_birthday = input("Your kid's birthday     : ")
     gender       = input("Your gender             : ")
     company      = input("Your company's name     : ")
